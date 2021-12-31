5a5e48d75d52daed8752dd7705e1a261319bc5fa
Priscilla Silva <priscilla.sg91@gmail.com>
2021-12-30 20:44:25 -0800

    done with js navbar

---
+++ script.js
@@ -0,0 +1,9 @@
+function openSideNav() {
+  document.getElementById("sidenav").style.width="250px";
+  document.getElementByClassName("main").style.marginLeft="250px";
+}
+
+function closeSideNav() {
+  document.getElementById("sidenav").style.width="0px";
+  document.getElementByClassName("main").style.marginLeft="0px";
+}
