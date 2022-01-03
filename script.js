0969d12322f065c024389b44ea1bd4f3851a8982
Priscilla Silva <priscilla.sg91@gmail.com>
2022-01-03 00:11:00 -0800

    done with vanillaJS version

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ function closeSideNav() {
   document.getElementById("sidenav").style.width="0px";
   document.getElementByClassName("main").style.marginLeft="0px";
 }
+
+var modalBg = document.querySelector('.modal-bg');
+var modal = document.querySelector('.modal');
+var btn = document.querySelector('.subscribe');
+var x = document.querySelector('.newsletter-closebtn');
+
+btn.addEventListener('click', function(){
+  modalBg.classList.add('bg-active');
+});
+
+x.addEventListener('click', function(){
+  modalBg.classList.remove('bg-active');
+});
+
